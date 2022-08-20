7dc5d1a547978f65bed2f95b9ea465896b92aeb0
jgm0327 <gmjang6349@gmail.com>
2022-08-20 15:00:36 +0900

    gitflow-feature-stash: #4-Entity
    DB에 저장할 객체 생성

---
+++ src/main/java/com/jgm/login/user/User.java
@@ -0,0 +1,37 @@
+package com.jgm.login.user;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
+
+@Entity
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    @Id
+    @Column(name = "user_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "user_name", unique = true, length = 50)
+    private String username;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "role")
+    private String role;
+
+    @CreationTimestamp
+    private Timestamp createAt;
+}
