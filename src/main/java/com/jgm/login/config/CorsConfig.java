610b9b2ff0b58ce20c6428e09e46f0eaa0cc3705
jgm0327 <gmjang6349@gmail.com>
2022-08-23 13:51:43 +0900

    #7 - 1 Jwt를 위한 config 설정
    CorsConfig로 다른 주소애서도 리소스에 접근할 수 있도록 함. SecurityConfig에서 SessionManagement를 stateless 상태로 만듦

---
+++ src/main/java/com/jgm/login/config/CorsConfig.java
@@ -0,0 +1,24 @@
+package com.jgm.login.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+public class CorsConfig{
+
+    @Bean
+    public CorsFilter corsFilter(){
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+        config.addAllowedOrigin("*");
+        source.registerCorsConfiguration("/api/**", config);
+
+        return new CorsFilter(source);
+    }
+}
